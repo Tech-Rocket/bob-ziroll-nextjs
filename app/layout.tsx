5662fa98ecb63a9a68e1a72bf93c060fa318f3a9
Tech-Rocket <beelawrence8@gmail.com>
2025-04-26 09:44:17 +0000

    Added shadcn ui to the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat_Alternates } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({
-  subsets: ['latin']
-})
+const montserrat_alt = Montserrat_Alternates({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "700", "800"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
+      <body className={`${montserrat_alt.className} antialiased`}>
         {children}
       </body>
     </html>
